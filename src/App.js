3921f7277ff472809aaac88b7ad94c3b72a9e02c
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-09-28 23:58:20 +0200

    New App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import React from 'react';
-import { Route, Switch } from "react-router-dom";
-import ListingPage from "./views/ListingPage";
-import './App.css';
+import React, { Component, useState } from 'react'
+import { GoogleMap, useLoadScript, Marker, InfoWindow, StreetViewPanorama } from "@react-google-maps/api";
+import { formatRelative } from "date-fns";
+import mapStyles from "./mapStyles"
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" component={ListingPage} />
-    </Switch>
-  );
+
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "50vw",
+  height: "100vh"
+}
+const center = {
+  lat: 41.3275,
+  lng: 19.8187
+}
+const options = {
+  styles: mapStyles.light,
+  disableDefaultUI: true,
+  // zoomControl: true
 }
 
-export default App;
+
+function App() {
+  const [markers, setMarkers] = React.useState([]);   //Adding Markers via onClick
+  const { isLoaded, loadError } = useLoadScript({ // this hook gives us back 2variables to know when our googlescript is righty so we can know to work with it 
+    googleMapsApiKey: "AIzaSyB6xHfPLxTArJQQzUVAs2EV6CZG6UT9HCU",
+    libraries,
+  })
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={12}
+        center={center}
+        options={options}
+        onClick={(event) => {
+          setMarkers(current => [...current, {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng(),
+            time: new Date(),
+          }])
+        }}
+      >
+        {markers.map(marker =>
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+          />)}
+
+      </GoogleMap>
+    </div>
+  )
+}
+
+export default App
