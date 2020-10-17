46e4cb4f591526cf1dd4151a6d7190a356a3916a
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-10-17 23:16:31 +0200

    Name Added

--- src/js/Card.js
+++ src/js/Card.js
@@ -2,21 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Card = ({ property, activeProperty, setActiveProperty }) => {
-  const { address, bathrooms, bedrooms, carSpaces, city, index, picture, price } = property;
+  const { address, bathrooms, bedrooms, carSpaces, city, index, picture, price, name } = property;
 
   return (
     <div id={`card-${index}`} className={`card col-sm-12 col-md-6 col-lg-6 ${property === activeProperty ? 'is-active' : ''}`} onClick={() => setActiveProperty(property, false)}>
       <img src={picture} alt={city} />
-      <p className="price">{price}</p>
+
+      <h5><b>{name}</b></h5>
       <div className="details">
-        <span className="index">{index + 1}</span>
+        {/* <span className="index">{index + 1}</span> */}
         <p className="location">
-          {city}<br />{address}
+          {address},{city}
         </p>
         <ul className="features">
           <li className="icon-bed">{bedrooms}<span>bedrooms</span></li>
           <li className="icon-bath">{bathrooms}<span>bathrooms</span></li>
           <li className="icon-car">{carSpaces}<span>parking spots</span></li>
+          <span className="price">ALL {price}<span>/month</span></span>
         </ul>
       </div>
     </div>)
